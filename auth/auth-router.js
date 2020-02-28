6fa5ecb9e5f1c02836ec2a7c8e1d4111e490e70f
jTCode2408 <jasmine.terry91@gmail.com>
2020-02-28 12:09:46 -0500

    pass hashed on register. token on login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,17 +1,20 @@
 const router = require('express').Router();
 const crypt = require('bcryptjs');
 const jToken = require('jsonwebtoken');
-
+const { jSecret } = require('../config/secrets');
 const Users = require('./users-model');
 
 
 router.post('/register', (req, res) => {
   const newUser = req.body
+  
   // implement registration
   //hash password then do req
+  const hash = crypt.hashSync(newUser.password, 10)
+  newUser.password = hash
 Users.add(newUser)
   .then(newest => {
-    res.status(201).json(newest)
+    res.status(201).json(newUser)
   })
   .catch(err => {
     console.log(err)
@@ -26,10 +29,14 @@ router.post('/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(loggedIn => {
-      if (username) {
-        res.status(200).json({Welcome: `${username}` })
+      if (loggedIn && crypt.compareSync(password, loggedIn.password)) {
+        const token = getToken(loggedIn)
+
+        res.status(200).json({
+          Welcome: `${username}`, token
+        })
       } else {
-        res.status(400).json({error:'creentials not found'})
+        res.status(401).json({error:'credentials not found'})
       }
     })
     .catch(err => {
@@ -37,8 +44,19 @@ router.post('/login', (req, res) => {
       res.status(500).json({error:"unable to login"})
     })
 
-
-
 });
 
+
+function getToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: "2h",
+  }
+
+  return jToken.sign(payload, jSecret, options)
+}
 module.exports = router;
